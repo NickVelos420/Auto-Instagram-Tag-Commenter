60222182b2381bd62ece126e013f8e2f511fe307
NickVelos420 <velosn@gmail.com>
2021-05-02 22:10:30 +0300

    added the checkIfCanComment func and check if it's working

--- instagram-commenter2.py
+++ instagram-commenter2.py
@@ -1,26 +1,15 @@
 import pyautogui as pya
 import string
 import random
-import keyboard as kb
+from functions.checkforError import checkIfCantComment
 from time import sleep
 
-commentInput = pya.locateOnScreen('comment.png', confidence=0.8)
+commentInput = pya.locateOnScreen('images/comment.png', confidence=0.8)
 
 # the center x and y value of the comment input 
 commentLocation = pya.center(commentInput)
 
 timesToGoDown = 1
-
-# The instagram algorithm stopes you from commenting after a bit so we have to if it has blocked us
-# when it stopes you it pops up a message so we this function just searches the sreen for that after 
-# it hit the enter button
-
-def checkIfCantComment():
-  checksForError = pya.locateOnScreen("error.png", confidence=0.5)
-  if checksForError != None:
-    sleep(4 * 60)
-  else: 
-    return
 
 def bot(): 
   global timesToGoDown 
@@ -38,8 +27,13 @@ def bot():
       pya.press("enter")
     pya.click(commentLocation)
     pya.press("enter")
+    # this sleep is here because the error takes some ms to pop up
+    sleep(1)
+    # takes a screenshot of the screen to see if the error.png file is contained within it
+    # the region will be different for you
+    pya.screenshot('images/screen.png')
     # checks if instagram blocked you from commenting  
-    checkIfCantComment()
+    checkIfCantComment("images/screen.png", "images/error.png")
     timesToGoDown += 1
     # this line is needed because when you post a comment it takes some time to load so it's based on your internet and it doesn't have to be that high you can change it
     sleep(3)
